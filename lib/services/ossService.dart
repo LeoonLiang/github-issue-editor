88f0077ffcdbc02fcb93279b869996245286acc9
Leoonliang <dsleoon@gmail.com>
2025-02-08 10:57:15 +0800

    feat: 新增七牛云上传

--- lib/services/ossService.dart
+++ lib/services/ossService.dart
@@ -2,31 +2,48 @@ import 'dart:io';
 import 'package:aws_s3_api/s3-2006-03-01.dart';
 
 class OssService {
-
   Future<void> uploadFileToS3(String filePath, String fileName) async {
-    // 创建S3客户端
+    // 创建S3客户端（缤纷）
     final credentials = AwsClientCredentials(
       accessKey: 'YOUR_BITIFUL_ACCESS_KEY',
       secretKey: 'YOUR_BITIFUL_SECRET_KEY',
     );
-
     final s3 = S3(
-      region: 'cn-east-1',
-      credentials: credentials,
-      endpointUrl: 'https://s3.bitiful.net'
+        region: 'cn-east-1',
+        credentials: credentials,
+        endpointUrl: 'https://s3.bitiful.net');
+
+    // 创建S3客户端（七牛）
+    final credentialsQiniu = AwsClientCredentials(
+      accessKey: 'YOUR_QINIU_ACCESS_KEY',
+      secretKey: 'YOUR_QINIU_SECRET_KEY',
     );
+    final s3Qiniu = S3(
+        region: 'cn-south-1',
+        credentials: credentialsQiniu,
+        endpointUrl: 'https://s3.cn-south-1.qiniucs.com');
 
     final file = File(filePath);
+    final fileBytes = file.readAsBytesSync();
 
     try {
-      // 使用S3客户端上传文件
-      final uploadResponse = await s3.putObject(
-        bucket: 'leoon-cn', // 你的S3桶名称
-        key: fileName,
-        body: file.readAsBytesSync(),
-        contentType: 'image/jpeg', // 根据文件类型选择
-      );
-      print('文件上传成功: ${uploadResponse.eTag}');
+      // 并行上传到两个 S3 端点
+      final responses = await Future.wait([
+        s3.putObject(
+          bucket: 'leoon-cn',
+          key: fileName,
+          body: fileBytes,
+          contentType: 'image/jpeg',
+        ),
+        s3Qiniu.putObject(
+          bucket: 'leoon-cn',
+          key: fileName,
+          body: fileBytes,
+          contentType: 'image/jpeg',
+        ),
+      ]);
+
+      print('文件上传成功: ${responses.map((res) => res.eTag).join(", ")}');
     } catch (e) {
       print('上传失败: $e');
     }
